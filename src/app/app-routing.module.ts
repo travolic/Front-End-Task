0ed9da6d15d91b6b28c748c12fdc46c15cd7e8ab
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-02-16 08:05:55 +0200

    Create lazy loaded hotels module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'hotels', loadChildren: () => import('./hotels/hotels.module').then(m => m.HotelsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hotels/hotels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ft-hotels',
+  templateUrl: './hotels.component.html',
+  styleUrls: ['./hotels.component.scss']
+})
+export class HotelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
